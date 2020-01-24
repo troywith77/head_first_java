94a724a3af4ede5b40e073653ef8a2d09c187123
troywith77 <ruitang307@gmail.com>
2020-01-24 09:40:16 +0800

    DooBee

--- Loopy.java
+++ Loopy.java
@@ -1,9 +1,9 @@
 public class Loopy {
-  public static void main(String args[]) {
+  public static void main(String[] args) {
     int x = 1;
     System.out.println("Before the loop");
     System.out.println("\n");
-    while(x < 4) {
+    while (x < 4) {
       System.out.println("In the loop");
       System.out.println("Value of x is " + x);
       x++;
